893159706366baa099df48395effeea5f307abda
hsy822 <hyunsy822@gmail.com>
2021-08-28 12:00:29 +0900

    Updates

---
+++ precache-manifest.9b9a5476562c91fa8a6a246dcfdfc379.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee857b4478379c1f9dec7671963c340",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f24ad34de3c3b292c24",
+    "url": "/static/css/main.b2b56b09.chunk.css"
+  },
+  {
+    "revision": "4a65e32dd3fefbf8c590",
+    "url": "/static/js/2.0a7aa783.chunk.js"
+  },
+  {
+    "revision": "2f24ad34de3c3b292c24",
+    "url": "/static/js/main.ac238993.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "868adf04f378b6d59b697cdf4e11deee",
+    "url": "/static/media/celo.868adf04.png"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "2bd3fbb3284112d869faf1ad44a08385",
+    "url": "/static/media/eth.2bd3fbb3.png"
+  },
+  {
+    "revision": "74d043f1e955c247d50d4722e27fe87f",
+    "url": "/static/media/function.74d043f1.PNG"
+  },
+  {
+    "revision": "ca29c2761e2c9e0f7a8278b7cd1b9672",
+    "url": "/static/media/json.ca29c276.PNG"
+  },
+  {
+    "revision": "1cf2fa4e69fb63e4754824502c747c32",
+    "url": "/static/media/klaytntr.1cf2fa4e.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "1ee4a1ab4dd95100e8d21e67acf53849",
+    "url": "/static/media/polygon.1ee4a1ab.png"
+  },
+  {
+    "revision": "ce624a36ce09d6bfbdbd479f67bb66ad",
+    "url": "/static/media/relayer.ce624a36.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/static/media/square5.ff004a0e.png"
+  }
+]);
